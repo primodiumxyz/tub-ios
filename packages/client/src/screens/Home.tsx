396553120e1772146bfb04859ba290f9212c086c
Emerson Hsieh <emersonhsieh@berkeley.edu>
2024-08-15 23:50:41 -0700

    feat: read wallet address from react

--- packages/client/src/screens/Home.tsx
+++ packages/client/src/screens/Home.tsx
@@ -1,9 +1,22 @@
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 export const Home = () => {
-  return (
-    <div>
-      <WalletMultiButton />
-    </div>
-  );
+  const { publicKey } = useWallet();
+
+  if (!publicKey) {
+    return (
+      <div>
+        Please connect a wallet.
+        <WalletMultiButton />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Connected wallet: {publicKey.toString()}</p>
+        <WalletMultiButton />
+      </div>
+    );
+  }
 };
