a8f45b6a67c3d1fc121251792e87fe4e48ee4b2b
Emerson Hsieh <emersonhsieh@berkeley.edu>
2024-08-16 22:28:38 -0700

    feat: sample counter contract

--- packages/contracts/programs/tub/src/lib.rs
+++ packages/contracts/programs/tub/src/lib.rs
@@ -7,10 +7,42 @@ pub mod tub {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &ctx.accounts.counter;
+        msg!("Counter account created! Current count: {}", counter.count);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented! New count: {}", counter.count);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 8
+    )]
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
--- packages/contracts/tests/tub.ts
+++ packages/contracts/tests/tub.ts
@@ -1,16 +1,47 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Tub } from "../target/types/tub";
+import { Keypair } from "@solana/web3.js";
 
 describe("tub", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Tub as Program<Tub>;
+  const counterAccount = new Keypair();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    // Invoke the initialize instruction
+    const transactionSignature = await program.methods
+      .initialize()
+      .accounts({
+        counter: counterAccount.publicKey,
+      })
+      .signers([counterAccount]) // include counter keypair as additional signer
+      .rpc({ skipPreflight: true });
+
+    // Fetch the counter account data
+    const accountData = await program.account.counter.fetch(
+      counterAccount.publicKey
+    );
+
+    console.log(`Transaction Signature: ${transactionSignature}`);
+    console.log(`Count: ${accountData.count}`);
+  });
+
+  it("Increment", async () => {
+    const transactionSignature = await program.methods
+      .increment()
+      .accounts({
+        counter: counterAccount.publicKey,
+      })
+      .rpc();
+
+    const accountData = await program.account.counter.fetch(
+      counterAccount.publicKey
+    );
+
+    console.log(`Transaction Signature: ${transactionSignature}`);
+    console.log(`Count: ${accountData.count}`);
   });
 });
