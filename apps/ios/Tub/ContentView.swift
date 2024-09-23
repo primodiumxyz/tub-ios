846e2d43907f7ffa42f19d8e0acc2fcada7d0443
Emerson Hsieh <emersonhsieh@berkeley.edu>
2024-09-24 00:57:20 +0800

    chore: add swift project

---
+++ apps/ios/Tub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tub
+//
+//  Created by Emerson Hsieh on 2024/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
