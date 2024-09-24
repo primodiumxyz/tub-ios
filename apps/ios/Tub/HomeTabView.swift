c80de2e4789630bd112eeeb9a941e190221f3b67
Emerson Hsieh <emersonhsieh@berkeley.edu>
2024-09-25 00:31:24 +0800

    feat: tab view

---
+++ apps/ios/Tub/HomeTabView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeTabView.swift
+//  Tub
+//
+//  Created by Emerson Hsieh on 2024/9/24.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+            Text("Tab Content 3").tabItem { Text("Tab Label 3") }.tag(2)
+        }
+    }
+}
+
+#Preview {
+    HomeTabView()
+}
