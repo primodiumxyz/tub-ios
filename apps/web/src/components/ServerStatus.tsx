a5f98fd3711d5a1b508ec5b189fcf7d3a5e0bd50
0xhank <zeroxhank@gmail.com>
2024-09-24 12:08:35 -0400

    feat: add monkey coins screen

--- apps/web/src/components/ServerStatus.tsx
+++ apps/web/src/components/ServerStatus.tsx
@@ -3,21 +3,32 @@ import { useServer } from "../hooks/useServer";
 
 export default function IncrementForm() {
   const { ready, server } = useServer();
-  const [status, setStatus] = useState<number>();
+  const [status, setStatus] = useState<number | undefined>(undefined);
 
   useEffect(() => {
     if (!ready) return;
 
     (async () => {
-      const status = await server?.getStatus.query();
-      setStatus(status?.status ?? 404);
+      try {
+        const status = await server?.getStatus.query();
+        setStatus(status?.status ?? 404);
+      } catch (e) {
+        console.log({ e });
+        setStatus(404);
+      }
     })();
   }, [server, ready]);
 
   return (
     <div className="absolute top-2 left-2 shadow-md flex flex-row justify-center gap-10 items-center bg-slate-300 rounded-xl p-2">
       <p className="text-md">Server Status</p>
-      <p className="text-md bg-slate-400 rounded-md p-2">{status}</p>
+      <p
+        className={`text-md ${
+          status === undefined ? "bg-slate-400" : status === 200 ? "bg-green-400" : "bg-red-400"
+        } rounded-md p-2`}
+      >
+        {status}
+      </p>
     </div>
   );
 }
