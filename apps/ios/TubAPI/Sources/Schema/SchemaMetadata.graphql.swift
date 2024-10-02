693ce947726f96e4c3e76df0eccde59f9e210023
0xhank <zeroxhank@gmail.com>
2024-10-02 14:33:06 -0400

    feat: add queries

--- apps/ios/TubAPI/Sources/Schema/SchemaMetadata.graphql.swift
+++ apps/ios/TubAPI/Sources/Schema/SchemaMetadata.graphql.swift
@@ -22,6 +22,14 @@ public enum SchemaMetadata: ApolloAPI.SchemaMetadata {
     switch typename {
     case "query_root": return TubAPI.Objects.Query_root
     case "account": return TubAPI.Objects.Account
+    case "token": return TubAPI.Objects.Token
+    case "account_transaction_aggregate": return TubAPI.Objects.Account_transaction_aggregate
+    case "account_transaction_aggregate_fields": return TubAPI.Objects.Account_transaction_aggregate_fields
+    case "account_transaction_sum_fields": return TubAPI.Objects.Account_transaction_sum_fields
+    case "token_transaction_aggregate": return TubAPI.Objects.Token_transaction_aggregate
+    case "token_transaction_aggregate_fields": return TubAPI.Objects.Token_transaction_aggregate_fields
+    case "token_transaction_sum_fields": return TubAPI.Objects.Token_transaction_sum_fields
+    case "token_price_history": return TubAPI.Objects.Token_price_history
     default: return nil
     }
   }
