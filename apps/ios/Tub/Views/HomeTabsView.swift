dacf849677cb69f071a0b9cb5b1298530f40bfb3
0xhank <zeroxhank@gmail.com>
2024-10-02 13:31:36 -0400

    fix: infinite rerender

--- apps/ios/Tub/Views/HomeTabsView.swift
+++ apps/ios/Tub/Views/HomeTabsView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct HomeTabsView: View {
     var body: some View {
         
-        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+        TabView() {
             CollectionView().tabItem {
                 Label("Collection", systemImage: "list.dash")
-            }.tag(1)
+            }
             MessageView().tabItem {
                 Label("Message", systemImage: "list.dash")
-            }.tag(2)
+            }
             CoinView().tabItem {
                 Label("Explore", systemImage: "house")
-            }.tag(3)
+            }
         }
     }
 }
