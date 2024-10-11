c917c1682af2522176109c96806cc9788fec25c1
Nabs <43505437+NAB5@users.noreply.github.com>
2024-10-11 11:43:20 -0400

    fix: remove print

--- apps/ios/Tub/Sources/Views/History/HistoryView.swift
+++ apps/ios/Tub/Sources/Views/History/HistoryView.swift
@@ -46,8 +46,6 @@ struct HistoryView : View {
                             let price = transaction.token_price?.price ?? 0
                             let value = (Double(price)/1e9) * (quantity)
                             
-                            print(transaction.amount.dividedReportingOverflow(by: 10000000000))
-                            
                             let newTransaction = Transaction(
                                 name: name,
                                 symbol: symbol,
