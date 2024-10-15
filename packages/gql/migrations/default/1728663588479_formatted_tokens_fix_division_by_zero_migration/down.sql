ba4be1d101d5c4a1d84ab1c95080109805cf3e42
polarzero <0xpolarzero@gmail.com>
2024-10-15 17:19:35 +0200

    fix: down migration

--- packages/gql/migrations/default/1728663588479_formatted_tokens_fix_division_by_zero_migration/down.sql
+++ packages/gql/migrations/default/1728663588479_formatted_tokens_fix_division_by_zero_migration/down.sql
@@ -1,1 +1,24 @@
-DROP VIEW IF EXISTS "public"."formatted_tokens";
+CREATE OR REPLACE VIEW "public"."formatted_tokens" AS 
+ WITH latest_token_stats AS (
+         SELECT DISTINCT ON (t.mint) t.id AS token_id,
+            t.mint,
+            t.name,
+            t.symbol,
+            count(*) OVER (PARTITION BY t.mint) AS trades,
+            first_value(tph.price) OVER (PARTITION BY t.mint ORDER BY tph.created_at DESC) AS latest_price,
+            first_value(tph.price) OVER (PARTITION BY t.mint ORDER BY tph.created_at) AS initial_price,
+            max(tph.created_at) OVER (PARTITION BY t.mint) AS latest_created_at
+           FROM (token_price_history tph
+             JOIN token t ON ((tph.token = t.id)))
+          WHERE (t.mint IS NOT NULL)
+          ORDER BY t.mint, tph.created_at DESC
+        )
+ SELECT latest_token_stats.token_id,
+    latest_token_stats.mint,
+    latest_token_stats.name,
+    latest_token_stats.symbol,
+    latest_token_stats.latest_price,
+    ((((latest_token_stats.latest_price - latest_token_stats.initial_price) / latest_token_stats.initial_price) * (100)::numeric))::double precision AS increase_pct,
+    latest_token_stats.trades,
+    latest_token_stats.latest_created_at AS created_at
+   FROM latest_token_stats;
