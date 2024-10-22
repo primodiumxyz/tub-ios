2179f8025f5a533ee3006133ab21c929e2260354
polarzero <0xpolarzero@gmail.com>
2024-10-22 18:27:03 +0200

    wip: debug crashing app

--- apps/ios/Tub/Sources/Views/Token/TokenListView.swift
+++ apps/ios/Tub/Sources/Views/Token/TokenListView.swift
@@ -211,6 +211,8 @@ struct TokenListView: View {
                     switch result {
                     case .success(let graphQLResult):
                         if let tokens = graphQLResult.data?.token {
+                            print(tokens)
+                            // TODO: this crashes the app
                             self.tokens = tokens.map { elem in Token(id: elem.id, name: elem.name, symbol: elem.symbol, imageUri: elem.uri) }
                             updateTokenModel(tokenId: tokens[0].id)
                             previousTokenModel = getPreviousTokenModel()
