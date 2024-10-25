d0a1966f104b70e573b17b1e182910ba14542e79
polarzero <0xpolarzero@gmail.com>
2024-10-25 18:12:31 +0200

    refactor: better format analytics

--- apps/dashboard/src/components/analytics/index.tsx
+++ apps/dashboard/src/components/analytics/index.tsx
@@ -14,11 +14,13 @@ export const Analytics = () => {
 
   if (error) return <div>{error}</div>;
   return (
-    <div className="flex flex-col items-start w-full max-h-fit">
-      <h3 className="text-lg font-semibold">Analytics</h3>
-      <div className="flex gap-2">
-        <DateRangePicker />
-        <DatePresetsPicker />
+    <div className="flex flex-col items-start w-full max-h-fit gap-4">
+      <div className="flex justify-between gap-4 w-full">
+        <h3 className="text-lg font-semibold">Analytics</h3>
+        <div className="flex gap-2">
+          <DateRangePicker />
+          <DatePresetsPicker />
+        </div>
       </div>
       <Table className="text-left w-full">
         <TableCaption>Swaps and new tokens over a period of {formatDistance(from, to)}.</TableCaption>
@@ -27,11 +29,17 @@ export const Analytics = () => {
             <TableHead className="w-[100px]">Category</TableHead>
             <TableHead>Total</TableHead>
             {swaps?.map((swap) => (
-              <TableHead>
-                {swaps.indexOf(swap) === 0 ||
-                new Date(swap.hour ?? "").getDay() !== new Date(swaps[swaps.indexOf(swap) - 1].hour ?? "").getDay()
-                  ? new Date(swap.hour ?? "").toLocaleString()
-                  : new Date(swap.hour ?? "").toLocaleTimeString()}
+              <TableHead className="h-full">
+                <div className="flex flex-col">
+                  {swaps.indexOf(swap) === 0 ||
+                  new Date(swap.hour ?? "").getDay() !==
+                    new Date(swaps[swaps.indexOf(swap) - 1].hour ?? "").getDay() ? (
+                    <span>{new Date(swap.hour ?? "").toLocaleDateString()}</span>
+                  ) : (
+                    <span className="opacity-0">-</span>
+                  )}
+                  <span>{new Date(swap.hour ?? "").toLocaleTimeString()}</span>
+                </div>
               </TableHead>
             ))}
           </TableRow>
@@ -39,35 +47,39 @@ export const Analytics = () => {
         <TableBody>
           <TableRow>
             <TableCell className="font-medium">Swaps</TableCell>
-            <TableCell>
-              {totalSwaps?.toLocaleString()}{" "}
+            <TableCell className="flex flex-col">
+              {totalSwaps?.toLocaleString()}
               <span className="text-muted-foreground">
                 {totalSwaps ? (totalSwaps / (3600 * (swaps?.length ?? 1))).toLocaleString() : 0}/s
               </span>
             </TableCell>
-            {swaps?.map((swap) => (
-              <TableCell>
-                {swap.count?.toLocaleString()}{" "}
-                <span className="text-muted-foreground">
-                  {swap.count ? (Number(swap.count) / 3600).toLocaleString() : 0}/s
-                </span>
+            {swaps?.map((swap, i) => (
+              <TableCell key={i}>
+                <div className="flex flex-col">
+                  {swap.count?.toLocaleString()}
+                  <span className="text-muted-foreground">
+                    {swap.count ? (Number(swap.count) / 3600).toLocaleString() : 0}/s
+                  </span>
+                </div>
               </TableCell>
             ))}
           </TableRow>
           <TableRow>
             <TableCell className="font-medium">New tokens</TableCell>
-            <TableCell>
-              {totalNewTokens?.toLocaleString()}{" "}
+            <TableCell className="flex flex-col">
+              {totalNewTokens?.toLocaleString()}
               <span className="text-muted-foreground">
                 {totalNewTokens ? (totalNewTokens / (3600 * (newTokens?.length ?? 1))).toLocaleString() : 0}/s
               </span>
             </TableCell>
-            {newTokens?.map((newToken) => (
-              <TableCell>
-                {newToken.count?.toLocaleString()}{" "}
-                <span className="text-muted-foreground">
-                  {newToken.count ? (Number(newToken.count) / 3600).toLocaleString() : 0}/s
-                </span>
+            {newTokens?.map((newToken, i) => (
+              <TableCell key={i}>
+                <div className="flex flex-col">
+                  {newToken.count?.toLocaleString()}{" "}
+                  <span className="text-muted-foreground">
+                    {newToken.count ? (Number(newToken.count) / 3600).toLocaleString() : 0}/s
+                  </span>
+                </div>
               </TableCell>
             ))}
           </TableRow>
