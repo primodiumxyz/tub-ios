01f5c6bf694314c2346f66d7c4b1d949036b6779
polarzero <0xpolarzero@gmail.com>
2024-10-30 16:50:43 +0100

    fix: last type errors

--- apps/dashboard/src/components/data-analysis/performance-data.tsx
+++ apps/dashboard/src/components/data-analysis/performance-data.tsx
@@ -1,14 +1,11 @@
-import { formatDistance } from "date-fns";
-
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { useAnalyticsParams } from "@/hooks/use-analytics-params";
+// import { useAnalyticsParams } from "@/hooks/use-analytics-params";
 import { useDataAnalysisData } from "@/hooks/use-data-analysis";
 
 // Total average performance after the period has ended
 // Average performance per interval during the period, using the same chart as the analytics
 export const PerformanceBasePeriodDataTable = () => {
   const { data } = useDataAnalysisData();
-  const { from, to } = useAnalyticsParams();
+  // const { from, to } = useAnalyticsParams();
   console.log(data);
 
   return null;
