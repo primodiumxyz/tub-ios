9646670bef2fbf2447e9b2e56b2098491880fa80
0xhank <zeroxhank@gmail.com>
2024-11-05 09:21:13 -0500

    feat: add view

---
+++ apps/ios/Tub/Sources/Views/Onramp/CoinbaseOnrampView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinbaseOnramp.swift
+//  Tub
+//
+//  Created by Henry on 11/5/24.
+//
+
+import SwiftUI
+
+struct CoinbaseOnrampView : View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+#Preview {
+    CoinbaseOnrampView()
+}
