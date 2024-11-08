b4ba838035acafb45bf456bbe3b7838e87009473
polarzero <0xpolarzero@gmail.com>
2024-11-07 18:42:11 -0700

    fix: stats

--- apps/ios/Tub/Sources/Models/TokenModel.swift
+++ apps/ios/Tub/Sources/Models/TokenModel.swift
@@ -230,14 +230,12 @@ class TokenModel: ObservableObject {
 
     func getTokenStats(priceModel: SolPriceModel) -> [(String, String)] {
         let currentPrice = prices.last?.price ?? 0
-        if currentPrice == 0 { return [] }
-        
-        let marketCap = currentPrice * token.supply / Int(pow(10.0, Double(token.decimals)))
+        let marketCap = Double(token.supply) / pow(10.0, Double(token.decimals)) * Double(currentPrice) // we're dividing first otherwise it will overflow...
         let volumeValue = Double(token.volume.value) / 1e9
         let supplyValue = Double(token.supply) / pow(10.0, Double(token.decimals))
         
         return [
-            ("Market Cap", priceModel.formatPrice(lamports: marketCap)),
+            ("Market Cap", priceModel.formatPrice(lamports: Int(marketCap))),
             ("Volume (\(String(token.volume.interval)))", formatLargeNumber(volumeValue)),
             ("Holders", "53.3K"), // TODO: Add holders data
             ("Supply", formatLargeNumber(supplyValue))
