42c08cedff01bb7491371e443121476f9783f8f1
Nabs <43505437+NAB5@users.noreply.github.com>
2024-11-08 12:06:21 -0700

    tests: fix flaky sub tests

--- packages/gql/__test__/subscription.test.ts
+++ packages/gql/__test__/subscription.test.ts
@@ -1,5 +1,6 @@
 import { beforeAll, describe, expect, it } from "vitest";
-import { createClient, GqlClient } from "../src/index"
+
+import { createClient, GqlClient } from "../src/index";
 import { createWallet } from "./lib/common";
 
 const token_id = "722e8490-e852-4298-a250-7b0a399fec57";
@@ -11,14 +12,13 @@ describe("query tests", () => {
     gql = await createClient({ url: "http://localhost:8080/v1/graphql", hasuraAdminSecret: "password" });
   });
 
-
   it("it should be able to subscribe to the wallet balance", async () => {
     const wallet = createWallet();
     const result = await gql.db.AirdropNativeToWalletMutation({ amount: "100", wallet });
     expect(result.data?.insert_wallet_transaction_one?.id).toBeDefined();
 
     let balance: number = 100;
-    gql.db.GetWalletBalanceSubscription({ wallet }).subscribe(data => {
+    gql.db.GetWalletBalanceSubscription({ wallet }).subscribe((data) => {
       balance = Number(data.data?.balance[0].value);
     });
 
@@ -28,8 +28,8 @@ describe("query tests", () => {
       wallet,
       amount: "100",
       override_token_price: "1000000000",
-      token: token_id
-    })
+      token: token_id,
+    });
 
     await new Promise((resolve) => setTimeout(resolve, 500));
 
@@ -38,36 +38,38 @@ describe("query tests", () => {
 
   it("it should be able to subscribe to the wallet balance before and after a transaction", async () => {
     const wallet = createWallet();
+
+    let balance: number[] = [];
+    let balanceBefore: number[] = [];
+
+    const subBalanceBefore = gql.db
+      .GetWalletBalanceIgnoreIntervalSubscription({ wallet, interval: "2000ms" })
+      .subscribe((data) => {
+        balanceBefore.push(Number(data.data?.balance[0].value));
+      });
+
+    const subBalance = gql.db.GetWalletBalanceSubscription({ wallet }).subscribe((data) => {
+      balance.push(Number(data.data?.balance[0].value));
+    });
+
     const result = await gql.db.AirdropNativeToWalletMutation({ amount: "100", wallet });
     expect(result.data?.insert_wallet_transaction_one?.id).toBeDefined();
+    await new Promise((resolve) => setTimeout(resolve, 500));
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    let balance: number = 100;
-    let balanceBefore: number = 100;
-
-    const subBalanceBefore = gql.db.GetWalletBalanceIgnoreIntervalSubscription({ wallet, interval: "500ms" }).subscribe(data => {
-      balanceBefore = Number(data.data?.balance[0].value);
-    });
-
-    const subBalance = gql.db.GetWalletBalanceSubscription({ wallet }).subscribe(data => {
-      balance = Number(data.data?.balance[0].value);
-    });
-
-    expect(balance).toBe(100);
-    expect(balanceBefore).toBe(100);
+    expect(balance).toEqual([100]);
+    expect(balanceBefore).toEqual([0]);
 
     await gql.db.BuyTokenMutation({
       wallet,
       amount: "100",
       override_token_price: "1000000000",
-      token: token_id
-    })
+      token: token_id,
+    });
 
     await new Promise((resolve) => setTimeout(resolve, 500));
 
-    expect(balance).toBe(0);
-    expect(balanceBefore).toBe(100);
+    expect(balance).toEqual([100, 0]);
+    expect(balanceBefore).toEqual([0]);
 
     subBalanceBefore.unsubscribe();
     subBalance.unsubscribe();
@@ -79,7 +81,7 @@ describe("query tests", () => {
     expect(result.data?.insert_wallet_transaction_one?.id).toBeDefined();
 
     let balance: number = 0;
-    const subBalance = gql.db.GetWalletTokenBalanceSubscription({ token: token_id, wallet }).subscribe(data => {
+    const subBalance = gql.db.GetWalletTokenBalanceSubscription({ token: token_id, wallet }).subscribe((data) => {
       balance = Number(data.data?.balance[0].value);
     });
 
@@ -89,10 +91,10 @@ describe("query tests", () => {
       wallet,
       amount: "100",
       override_token_price: "1000000000",
-      token: token_id
-    })
+      token: token_id,
+    });
 
-    await new Promise((resolve) => setTimeout(resolve, 500));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     expect(balance).toBe(100);
 
@@ -101,37 +103,40 @@ describe("query tests", () => {
 
   it("it should be able to subscribe to the token balance before and after a transaction", async () => {
     const wallet = createWallet();
-    const result = await gql.db.AirdropNativeToWalletMutation({ amount: "200", wallet });
+
+    let balance: number[] = [];
+    let balanceBefore: number[] = [];
+
+    const subBalanceBefore = gql.db
+      .GetWalletTokenBalanceIgnoreIntervalSubscription({ token: token_id, wallet, interval: "2s" })
+      .subscribe((data) => {
+        balanceBefore.push(Number(data.data?.balance[0].value));
+      });
+
+    const subBalance = gql.db.GetWalletTokenBalanceSubscription({ token: token_id, wallet }).subscribe((data) => {
+      balance.push(Number(data.data?.balance[0].value));
+    });
+
+    const result = await gql.db.AirdropNativeToWalletMutation({ amount: "100", wallet });
     expect(result.data?.insert_wallet_transaction_one?.id).toBeDefined();
+    await new Promise((resolve) => setTimeout(resolve, 500));
 
-    let balance: number = 0;
-    let balanceBefore: number = 0;
-
-    const subBalanceBefore = gql.db.GetWalletTokenBalanceIgnoreIntervalSubscription({ token: token_id, wallet, interval: "1s" }).subscribe(data => {
-      balanceBefore = Number(data.data?.balance[0].value);
-    });
-
-    const subBalance = gql.db.GetWalletTokenBalanceSubscription({ token: token_id, wallet }).subscribe(data => {
-      balance = Number(data.data?.balance[0].value);
-    });
-
-    expect(balance).toBe(0);
-    expect(balanceBefore).toBe(0);
+    expect(balance).toEqual([0]);
+    expect(balanceBefore).toEqual([0]);
 
     await gql.db.BuyTokenMutation({
       wallet,
       amount: "100",
       override_token_price: "1000000000",
-      token: token_id
-    })
+      token: token_id,
+    });
 
     await new Promise((resolve) => setTimeout(resolve, 500));
 
-    expect(balance).toBe(100);
-    expect(balanceBefore).toBe(0);
+    expect(balance).toEqual([0, 100]);
+    expect(balanceBefore).toEqual([0]);
 
     subBalanceBefore.unsubscribe();
     subBalance.unsubscribe();
   });
-
 });
