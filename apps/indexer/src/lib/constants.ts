ba56ab5f18af3cc8cd50c2e8f445b48531fad08e
polarzero <0xpolarzero@gmail.com>
2024-11-11 10:50:11 -0700

    wip: drop queue

--- apps/indexer/src/lib/constants.ts
+++ apps/indexer/src/lib/constants.ts
@@ -10,6 +10,7 @@ import { PublicKey } from "@solana/web3.js";
 export const FETCH_DATA_BATCH_SIZE = 50; // this is the max batch size (50 * 2 accounts)
 export const FETCH_HELIUS_WRITE_GQL_BATCH_SIZE = 20; // max 1k because we're fetching `getAssetBatch` from Helius and it has a limit of 1k
 export const PRICE_PRECISION = 1e9;
+export const MAX_QUEUE_DELAY_MS = 60_000; // if the indexer gets too far behind, just drop the queue and get back to the latest data to not spend minutes getting back up to speed
 
 export const WRAPPED_SOL_MINT = new PublicKey("So11111111111111111111111111111111111111112");
 export const PUMP_FUN_AUTHORITY = new PublicKey("TSLvdd1pWpHVjahSpsvCXUbgwsL3JAcvokwaKt1eokM");
