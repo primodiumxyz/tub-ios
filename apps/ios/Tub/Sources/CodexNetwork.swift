ddf5b2c87fc86ff070f4d6eacd8a8c393c62eb0f
polarzero <0xpolarzero@gmail.com>
2024-11-14 09:35:54 -0700

    feat: config for codex api

---
+++ apps/ios/Tub/Sources/CodexNetwork.swift
@@ -0,0 +1,38 @@
+//
+//  CodexNetwork.swift
+//  Tub
+//
+//  Created by polarzero on 14/11/2024.
+//
+
+import Apollo
+import Foundation
+
+class CodexNetwork {
+    static let shared = CodexNetwork()
+    
+    private let apiKey: String
+    
+    private(set) lazy var apollo: ApolloClient = {
+        let store = ApolloStore()
+        
+        let interceptorProvider = DefaultInterceptorProvider(store: store)
+        
+        // Create custom interceptor chain to add API key
+        let interceptor = RequestChainNetworkTransport(
+            interceptorProvider: interceptorProvider,
+            endpointURL: URL(string: "https://graph.codex.io/graphql")!,
+            additionalHeaders: [
+                "Authorization": apiKey,
+                "Content-Type": "application/json"
+            ]
+        )
+        
+        return ApolloClient(networkTransport: interceptor, store: store)
+    }()
+    
+    init() {
+        // In production, load from secure storage/environment
+        self.apiKey = "<MY_KEY>"
+    }
+}
