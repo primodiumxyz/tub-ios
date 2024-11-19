94efc449a82a3ab31f08c232d948dc6f547634b9
polarzero <0xpolarzero@gmail.com>
2024-11-19 18:15:33 +0100

    refactor: rank tokens by 1h volume

--- apps/ios/CodexAPI/Sources/Operations/Queries/GetFilterTokensQuery.graphql.swift
+++ apps/ios/CodexAPI/Sources/Operations/Queries/GetFilterTokensQuery.graphql.swift
@@ -7,14 +7,14 @@ public class GetFilterTokensQuery: GraphQLQuery {
   public static let operationName: String = "GetFilterTokens"
   public static let operationDocument: ApolloAPI.OperationDocument = .init(
     definition: .init(
-      #"query GetFilterTokens($rankingAttribute: TokenRankingAttribute = trendingScore1, $limit: Int = 50) { filterTokens( filters: { exchangeId: ["6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P:1399811149"] trendingIgnored: false potentialScam: false } rankings: { attribute: $rankingAttribute, direction: DESC } limit: $limit ) { __typename results { __typename token { __typename address info { __typename name symbol description imageLargeUrl imageSmallUrl imageThumbUrl } socialLinks { __typename discord instagram telegram twitter website } } priceUSD liquidity marketCap volume1 pair { __typename id } } } }"#
+      #"query GetFilterTokens($rankingAttribute: TokenRankingAttribute = volume1, $limit: Int = 50) { filterTokens( filters: { exchangeId: ["6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P:1399811149"] trendingIgnored: false potentialScam: false } rankings: { attribute: $rankingAttribute, direction: DESC } limit: $limit ) { __typename results { __typename token { __typename address info { __typename name symbol description imageLargeUrl imageSmallUrl imageThumbUrl } socialLinks { __typename discord instagram telegram twitter website } } priceUSD liquidity marketCap volume1 pair { __typename id } } } }"#
     ))
 
   public var rankingAttribute: GraphQLNullable<GraphQLEnum<TokenRankingAttribute>>
   public var limit: GraphQLNullable<Int>
 
   public init(
-    rankingAttribute: GraphQLNullable<GraphQLEnum<TokenRankingAttribute>> = .init(.trendingScore1),
+    rankingAttribute: GraphQLNullable<GraphQLEnum<TokenRankingAttribute>> = .init(.volume1),
     limit: GraphQLNullable<Int> = 50
   ) {
     self.rankingAttribute = rankingAttribute
