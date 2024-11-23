c30297e91e84d7d7d26d8d3028f47373d2a8e5b2
0xhank <zeroxhank@gmail.com>
2024-11-22 19:17:30 -0500

    feat: merge release

---
+++ apps/ios/Tub/Sources/Extensions/ViewExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  ViewExtensions.swift
+//  Tub
+//
+//  Created by JJ on 11/20/24.
+//
+
+import SwiftUI
+
+extension View {
+    // Allows modifiers to be coonditionally applied to views
+    @ViewBuilder
+    func conditionalModifier<V: View>(condition: Bool, modifier: (Self) -> V) -> some View {
+        if condition {
+            modifier(self)
+        }
+        else {
+            self
+        }
+    }
+}
