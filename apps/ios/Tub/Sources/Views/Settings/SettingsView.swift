2452cc99ffd72cef6ad7fb2c67c1f4e4bc65cf3f
yixintan9 <yxtan88@gmail.com>
2024-12-04 11:07:18 -0500

    fix: navigation title

--- apps/ios/Tub/Sources/Views/Settings/SettingsView.swift
+++ apps/ios/Tub/Sources/Views/Settings/SettingsView.swift
@@ -143,14 +143,10 @@ struct SettingsView: View {
 
                 Spacer()
             }
-            .navigationBarTitleDisplayMode(.inline)
             .navigationBarBackButtonHidden(false)
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("Settings")
-                        .font(.sfRounded(size: .xl, weight: .semibold))
-                }
-
                 // Keep keyboard toolbar
                 ToolbarItem(placement: .keyboard) {
                     Button("Save") {
