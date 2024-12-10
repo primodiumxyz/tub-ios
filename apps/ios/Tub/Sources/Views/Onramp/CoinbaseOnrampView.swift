0e859f1e948a9941842b2034c5d0cb6e92f51d9d
yixintan9 <yxtan88@gmail.com>
2024-12-10 08:50:32 -0800

    feat: new ui

--- apps/ios/Tub/Sources/Views/Onramp/CoinbaseOnrampView.swift
+++ apps/ios/Tub/Sources/Views/Onramp/CoinbaseOnrampView.swift
@@ -11,51 +11,78 @@ import WebKit
 struct CoinbaseOnrampView: View {
     @State private var url: URL? = nil
     @EnvironmentObject private var userModel: UserModel
+    @EnvironmentObject private var priceModel: SolPriceModel
     @State private var amountString: String = ""
     @State private var amount: Double = 100.0  // Default amount
     @State private var isValidInput: Bool = true
     @State private var showInput: Bool = true
     @FocusState private var isAmountFocused: Bool
     @State private var showWebView: Bool = false
+    @Environment(\.dismiss) private var dismiss
 
     var body: some View {
-        VStack {
-            if showInput {
-                VStack(spacing: 20) {
-                    numberInput
-                    continueButton
-                }
-                .padding()
-            }
-            else {
-                LoadingView(identifier: "Coinbase onramp")
-            }
+        NavigationStack{
+            VStack {
+                if showInput {
+                    VStack(alignment: .center) {
+                        Spacer().frame(height: UIScreen.height(Layout.Spacing.md))
 
-            if userModel.walletAddress == nil {
-                Text("Connect a wallet to continue").font(.sfRounded()).foregroundStyle(.tubError)
-            }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(UIColor.systemBackground))
-        .ignoresSafeArea(.keyboard)
-        .onAppear {
-            isAmountFocused = true
-        }
-        .sheet(isPresented: $showWebView) {
-            if let url = url {
-                ZStack(alignment: .top) {
-                    HStack(alignment: .center, spacing: 0) {
-                        Rectangle()
-                            .foregroundStyle(.clear)
-                            .frame(width: 60, height: 3)
-                            .background(.tubNeutral)
-                            .cornerRadius(100)
+                        numberInput
+                        
+                        Text("Your Balance \(priceModel.formatPrice(usdc: userModel.balanceUsdc ?? 0))")
+                            .font(.sfRounded(size: .lg, weight: .medium))
+                            .foregroundStyle(.tubBuyPrimary)
+                        
+                        Spacer()
+                        
+                        if amountString.isEmpty {
+                            amountButtons
+                        } else {
+                            continueButton
+                        }
+                        
+                        if userModel.walletAddress == nil {
+                            Text("Connect a wallet to continue").font(.sfRounded()).foregroundStyle(.tubError)
+                        }
                     }
-                    .padding()
-                    .zIndex(2)
-
-                    WebView(url: url)
-                        .ignoresSafeArea()
+                }
+                else {
+                    LoadingView(identifier: "Coinbase onramp")
+                }
+                
+            }
+            .padding(UIScreen.width(Layout.Spacing.md))
+            .navigationTitle("Deposit")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { dismiss() }) {
+                        Image(systemName: "xmark")
+                            .foregroundStyle(.tubBuyPrimary)
+                            .padding(8)
+                    }
+                }
+            }
+            .background(Color(UIColor.systemBackground))
+            .onAppear {
+                isAmountFocused = true
+            }
+            .sheet(isPresented: $showWebView) {
+                if let url = url {
+                    ZStack(alignment: .top) {
+                        HStack(alignment: .center, spacing: 0) {
+                            Rectangle()
+                                .foregroundStyle(.clear)
+                                .frame(width: 60, height: 3)
+                                .background(.tubNeutral)
+                                .cornerRadius(100)
+                        }
+                        .padding()
+                        .zIndex(2)
+                        
+                        WebView(url: url)
+                            .ignoresSafeArea()
+                    }
                 }
             }
         }
@@ -68,7 +95,7 @@ struct CoinbaseOnrampView: View {
                 Text("$")
                     .font(.sfRounded(size: .xl4, weight: .bold))
 
-                TextField("", text: $amountString, prompt: Text("100").foregroundStyle(.tubBuyPrimary.opacity(0.3)))
+                TextField("", text: $amountString, prompt: Text("100.00").foregroundStyle(.tubNeutral))
                     .focused($isAmountFocused)
                     .keyboardType(.decimalPad)
                     .multilineTextAlignment(.leading)
@@ -102,7 +129,7 @@ struct CoinbaseOnrampView: View {
                         }
                     }
                     .font(.sfRounded(size: .xl5, weight: .bold))
-                    .foregroundStyle(isValidInput ? .tubBuyPrimary : .tubError)
+                    .foregroundStyle(isValidInput ? .tubText : .tubError)
                     .frame(minWidth: 50)
                     .fixedSize()
                 Spacer()
@@ -113,9 +140,11 @@ struct CoinbaseOnrampView: View {
 
     private var continueButton: some View {
         ContentButton(
-            backgroundColor: .tubPurple,
+            backgroundColor: .tubBuyPrimary,
             disabled: userModel.walletAddress == nil || amountString == "" || !isValidInput,
             action: {
+                isAmountFocused = false
+                
                 guard let walletAddress = userModel.walletAddress else { return }
                 let urlStr =
                     "https://pay.coinbase.com/buy?appId=70955045-7672-4640-b524-0a5aff9e074e&addresses={\"\(walletAddress)\":[\"solana\"]}&assets=[\"USDC\"]&presetFiatAmount=\(amount)"
@@ -127,12 +156,30 @@ struct CoinbaseOnrampView: View {
             HStack(alignment: .firstTextBaseline) {
                 Text("Deposit with")
                     .font(.sfRounded(size: .xl, weight: .semibold))
-                    .foregroundStyle(.white)
+                    .foregroundStyle(.tubTextInverted)
                     .padding(.trailing, -4)
-                Image("coinbase")
+                Image("Coinbase")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(height: 16)
+            }
+        }
+    }
+    
+    let amounts: [Int] = [50, 100, 200, 1000]
+    private var amountButtons: some View {
+        HStack(spacing: 10) {
+            ForEach(amounts, id: \.self) { amount in
+                CapsuleButton(
+                    text: "$\(amount)",
+                    textColor: .tubBuyPrimary,
+                    backgroundColor: .clear,
+                    borderColor: .tubAltSecondary,
+                    borderWidth: 1,
+                    action: {
+                        amountString = String(format: "%.2f", Double(amount))
+                    }
+                )
             }
         }
     }
@@ -172,5 +219,6 @@ struct WebView: UIViewRepresentable {
 #Preview {
     CoinbaseOnrampView()
         .environmentObject(UserModel.shared)
+        .environmentObject(SolPriceModel.shared)
         .preferredColorScheme(.light)
 }
