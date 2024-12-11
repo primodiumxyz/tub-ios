f873e3d8494aba910464cb05d94d5450ee683388
CometShock <cometshockcrypto@gmail.com>
2024-12-11 09:26:57 -0600

    fix: parseEnv coerce CI

--- apps/server/bin/parseEnv.ts
+++ apps/server/bin/parseEnv.ts
@@ -14,7 +14,7 @@ const commonSchema = z.object({
   OCTANE_BUY_FEE: z.coerce.number().default(100),
   OCTANE_SELL_FEE: z.coerce.number().default(0),
   OCTANE_MIN_TRADE_SIZE: z.coerce.number().default(15),
-  CI: z.boolean().default(false),
+  CI: z.coerce.boolean().default(false),
   TEST_USER_PRIVATE_KEY: z.string().default("set TEST_USER_PRIVATE_KEY in .env before running server tests"),
 
   JUPITER_URL: z.string(),
