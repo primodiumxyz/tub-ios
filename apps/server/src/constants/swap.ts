838075f17bf433ddf1a6a7773ea1489df86716c0
CometShock <cometshockcrypto@gmail.com>
2024-12-12 15:54:26 -0600

    fix: autoSlippageUSD calc and add min slippage

--- apps/server/src/constants/swap.ts
+++ apps/server/src/constants/swap.ts
@@ -4,5 +4,6 @@ export const AUTO_SLIPPAGE = true;
 export const MAX_AUTO_SLIPPAGE_BPS = 200; // 2%
 export const MAX_ACCOUNTS = 50;
 export const AUTO_SLIPPAGE_COLLISION_USD_VALUE = 1000; // 1000 USD
+export const MIN_SLIPPAGE_BPS = 50;
 
 export const AUTO_PRIORITY_FEE_MULTIPLIER = 3;
