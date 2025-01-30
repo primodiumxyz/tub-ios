9e5948af73122ac4fd682e55839d97a117e0d7c2
0xpolarzero <0xpolarzero@gmail.com>
2025-01-30 19:59:11 +0100

    refactor: remove commented out test

--- apps/ios/Tub/TubTests/ApolloTests.swift
+++ apps/ios/Tub/TubTests/ApolloTests.swift
@@ -16,30 +16,5 @@ class ApolloTests: XCTestCase {
         apolloClient = nil
     }
     
-    func testGetAllTokensQuery() throws {
-        let expectation = self.expectation(description: "Get All Tokens Query")
-        
-//        apolloClient.fetch(query: GetAllTokensQuery()) { result in
-//            switch result {
-//            case .success(let graphQLResult):
-//                XCTAssertNotNil(graphQLResult.data)
-//                if let tokens = graphQLResult.data?.token {
-//                    XCTAssertFalse(tokens.isEmpty, "Tokens array should not be empty")
-//                    if let firstToken = tokens.first {
-//                        XCTAssertNotNil(firstToken.id)
-//                        XCTAssertNotNil(firstToken.name)
-//                        XCTAssertNotNil(firstToken.symbol)
-//                        XCTAssertNotNil(firstToken.updated_at)
-//                    }
-//                } else {
-//                    XCTFail("No tokens data received")
-//                }
-//                expectation.fulfill()
-//            case .failure(let error):
-//                XCTFail("Apollo query failed: \(error.localizedDescription)")
-//            }
-//        }
-        
-        waitForExpectations(timeout: 5, handler: nil)
-    }
+    // Add tests here
 }
